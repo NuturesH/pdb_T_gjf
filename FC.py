3d842bcb909e36ec595c2c4d4d81d00cdb9f5353
HXHSir <32289361+NuturesH@users.noreply.github.com>
2019-04-10 16:48:35 +0800

    Update FC.py

--- FC.py
+++ FC.py
@@ -4,6 +4,7 @@ class FC：
 	def LFC(self):
 		import os
 		name = self.filename.split(".pdb")[0]
+                resname = "".join(list(name)[-1].lowder())
 		one = "antechamber -i {0}.pdb -fi pdb -o {0}.mol2 -fo mol2 -c bcc -s 2".format(name)
 		os.system(one)
 		two = "parmchk -i {0}.mol2 -f mol2 -o {0}.frcmod".format(name)
@@ -11,10 +12,10 @@ class FC：
 		fp = open("mod.tleap", 'w')
 		print >> fp, "tleap -f oldff/leaprc.ff99SB"
 		print >> fp, "source leaprc.gaff"
-		print >> fp, "SUS = loadmol2 {}.mol2".format(name)
-		print >> fp, "check SUS"
+		print >> fp, "{0} = loadmol2 {1}.mol2".format(resname, name)
+		print >> fp, "check {}".format(resname)
 		print >> fp, "loadamberparams {}.frcmod".format(name)
-		print >> fp, "saveoff SUS {}.lib".format(name)
+		print >> fp, "saveoff {} {}.lib".format(resname, name)
 		print >> fp, "quit"
 		fp.close()
 		three = "tleap -f mod.tleap"
